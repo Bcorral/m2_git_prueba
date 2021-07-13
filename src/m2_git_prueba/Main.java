07f3417a30c0f2ae0c233e4da5ca937728ac643f
Bcorral <beatrizcorralcampos@gmail.com>
2021-07-13 12:53:32 +0200

    #1 Subir cambios 😎

--- src/m2_git_prueba/Main.java
+++ src/m2_git_prueba/Main.java
@@ -3,8 +3,8 @@ package m2_git_prueba;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hola mundo");
+		System.out.println("cambios");
 	}
 
 }
