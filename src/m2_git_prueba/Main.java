d862fec57c35ee2faf1eec69b7797bd833423367
Bcorral <beatrizcorralcampos@gmail.com>
2021-07-13 13:24:29 +0200

    commit1

--- src/m2_git_prueba/Main.java
+++ src/m2_git_prueba/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
-		System.out.println("cambios");
+		System.out.println("comprobar cambios");
 	}
 
 }
